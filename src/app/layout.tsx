4ee94ce6209a683951b52bf8e19c8908dc3b8a0b
Julio Sanic <julio.sanic.gt.256@gmail.com>
2024-08-10 23:41:26 -0600

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <ThemeContextProvider>
         <ModalContextProvider>
           <body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
-import About from '@/sections/about/About'
-import Education from '@/sections/education/Education'
-import FloatMenu from '@/sections/float-menu/FloatMenu'
-import Footer from '@/sections/footer/Footer'
-import Header from '@/sections/header/Header'
-import Projects from '@/sections/projects/Projects'
-import Services from '@/sections/services/Services'
-import React from 'react'
+import About from "@/sections/about/About";
+import Education from "@/sections/education/Education";
+import FloatMenu from "@/sections/float-menu/FloatMenu";
+import Footer from "@/sections/footer/Footer";
+import Header from "@/sections/header/Header";
+import Projects from "@/sections/projects/Projects";
+import Services from "@/sections/services/Services";
+import { Metadata } from "next";
+import React from "react";
 
-
+export const metadata: Metadata = {
+  title: "Julio Sanic - Desarrollador Web",
+  description:
+    "Desarrollador web fullstack con experiencia en react y next.js.",
+  keywords:
+    "Desarrollador web, React, Next.js, Fullstack, MERN, TypeScript, react native",
+};
 
 export default function page() {
   return (
@@ -19,5 +26,5 @@ export default function page() {
       <Education />
       <FloatMenu />
     </>
-  )
+  );
 }
