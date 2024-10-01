0985fb6a92f6411fa133d57faf805a42323ed8b4
sanic16 <201222286@ingenieria.usac.edu.gt>
2024-10-01 10:52:10 -0600

    migration

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -36,10 +36,10 @@ export default async function page({
   const t = await getDictionary(lang);
   return (
     <>
-      <Header translations={t.header} lang={lang} />
-      <About translations={t.about} lang={lang} />
+      {/* <Header translations={t.header} lang={lang} />
+      <About translations={t.about} lang={lang} /> */}
       <Projects translations={t.projects} lang={language} />
-      <Services translations={t.services} />
+      {/* <Services translations={t.services} /> */}
       <Education translations={t.education} />
       <FloatMenu />
     </>
